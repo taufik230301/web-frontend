e8149cc0c1f623827b24c840519141a3310ff6f5
Misbahudin04 <misbahudinmunadi1@gmail.com>
2020-06-08 16:44:33 +0700

    try 1st

---
+++ src/components/event/event.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./event.css"
+
+
+class Event extends React.Component{
+    render(){
+        return <div id="tentang">
+            <div className="container">              
+                <div><h2>Tentang</h2>
+                    <h1>Apa Itu Patal?</h1>
+                    <p>Palembang digital dibentuk pada tanggal 21 april 2020 yang diinisiasi oleh Beberapa pelaku IT yang berasal dari Kota Palembang Sumatera Selatan. Palembang digital merupakan sebuah komunitas Digital yang meliputi Software engineer, Content creator , digital marketing dan pelaku digital lainnya yang berasal dari sumatera selatan.</p>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default Event;
---
+++ src/components/events/events.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./event.css";
+import Article from "../article/article";
+import {Container, Row, Col} from 'reactstrap';
+import article1 from "../../images/artikel1.jpeg"
+import article2 from "../../images/artikel2.jpeg"
+import article3 from "../../images/artikel3.jpeg"
+
+class Events extends React.Component{
+    render(){
+        return <div id="event">
+                <div className="acara">
+                <Container className="themed-container title " fluid>
+                    <h2>Acara diwaktu dekat</h2>
+                    <h1>Kegiatan Patal</h1>
+                    <Row className="cp">
+                        <Col  md="4" >
+                            <Article img={article1} title="Workshop dasar implementasi penggunaan GIT"/>
+                        </Col>
+                        <Col  md="4">
+                            <Article img={article2} title="ODP Bareng MYO 2.0 : “Online Dapat Pengetahuan” Berbagi Tips & Pengalaman..."/>
+                        </Col>
+                        <Col  md="4">
+                           <Article img={article3} title="Webinar AWS Code Build & Code Deploy"/>
+                        </Col>
+                    </Row>
+                </Container>
+                </div>
+                </div>
+    }
+}
+
+export default Events;
